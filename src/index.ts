5d6ce9f2be0ef7488a642735039a0aaca61bacf8
Filipe Melo <filipemelo032@gmail.com>
2020-08-27 10:22:47 -0300

    Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { Builder, By, Key, until, Capabilities } from 'selenium-webdriver';
+import chrome from 'selenium-webdriver/chrome';
+import chromedriver from 'chromedriver';
+
+(async function App() {
+  const service = new chrome.ServiceBuilder(chromedriver.path).build();
+  chrome.setDefaultService(service);
+  const driver = await new Builder()
+    .withCapabilities(Capabilities.chrome())
+    .build();
+  try {
+    await driver.get('https://web.whatsapp.com/');
+  } finally {
+    await driver.quit();
+  }
+})();
